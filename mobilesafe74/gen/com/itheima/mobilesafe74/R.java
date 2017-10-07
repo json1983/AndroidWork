aa6c2182ac857b306244e63d1972523d2a0f14b0
json1983 <215971146@qq.com>
2017-10-07 16:04:19 +0800

    55

--- mobilesafe74/gen/com/itheima/mobilesafe74/R.java
+++ mobilesafe74/gen/com/itheima/mobilesafe74/R.java
@@ -36,13 +36,17 @@ public final class R {
         public static final int one=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int rl_root=0x7f080000;
-        public static final int tv_version_name=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int gv_home=0x7f080000;
+        public static final int iv_icon=0x7f080003;
+        public static final int rl_root=0x7f080001;
+        public static final int tv_title=0x7f080004;
+        public static final int tv_version_name=0x7f080002;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
         public static final int activity_splash=0x7f030001;
+        public static final int gridview_item=0x7f030002;
     }
     public static final class menu {
         public static final int splash=0x7f070000;
--- mobilesafe74/src/com/itheima/mobilesafe74/activity/HomeActivity.java
+++ mobilesafe74/src/com/itheima/mobilesafe74/activity/HomeActivity.java
@@ -5,8 +5,19 @@ import com.itheima.mobilesafe74.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
+	private GridView gv_home;
+	private String[] mTitleStrs;
+	private int[] mDrawableIds;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -18,12 +29,54 @@ public class HomeActivity extends Activity {
 	}
 
 	private void initData() {
-		// TODO Auto-generated method stub
-		
+		// 准备数据(文字(9组),图片(9张))
+		mTitleStrs = new String[] { "手机防盗", "通信卫士", "软件管理", "进程管理", "流量统计",
+				"手机杀毒", "缓存清理", "高级工具", "设置中心" };
+		mDrawableIds = new int[] { R.drawable.home_safe,
+				R.drawable.home_callmsgsafe, R.drawable.home_apps,
+				R.drawable.home_taskmanager, R.drawable.home_netmanager,
+				R.drawable.home_trojan, R.drawable.home_sysoptimize,
+				R.drawable.home_tools, R.drawable.home_settings };
+		gv_home.setAdapter(new MyAdapter());
+
 	}
 
 	private void initUI() {
 		// TODO Auto-generated method stub
+		gv_home = (GridView) findViewById(R.id.gv_home);
+
+	}
+
+	class MyAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return mTitleStrs.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// 条目的总数 文字组数 == 图片张数
+			return mTitleStrs[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
 		
+			View view = (View) View.inflate(getApplicationContext(),
+					R.layout.gridview_item, null);
+			TextView tv_title=(TextView)findViewById(R.id.tv_title);
+			ImageView iv_icon=(ImageView)findViewById(R.id.iv_icon);
+			tv_title.setText(mTitleStrs[position]);
+			iv_icon.setBackgroundResource(mDrawableIds[position]);
+			return view;
+		}
 	}
 }
