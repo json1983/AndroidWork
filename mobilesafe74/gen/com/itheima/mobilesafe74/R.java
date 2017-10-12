154258b88b0d70bcb48807359d0f0a4843dc28f9
dfdf <dddd>
2017-10-12 21:43:56 +0800

    444

--- mobilesafe74/gen/com/itheima/mobilesafe74/R.java
+++ mobilesafe74/gen/com/itheima/mobilesafe74/R.java
@@ -60,15 +60,17 @@ containing a value of this type.
         public static final int one=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f08000c;
         public static final int bt_cancel=0x7f080006;
         public static final int bt_submit=0x7f080005;
+        public static final int cb_box=0x7f08000b;
         public static final int et_confirm_psd=0x7f080004;
         public static final int et_set_psd=0x7f080007;
         public static final int gv_home=0x7f080000;
         public static final int iv_icon=0x7f080008;
         public static final int rl_root=0x7f080002;
         public static final int siv_update=0x7f080001;
+        public static final int tv_des=0x7f08000a;
         public static final int tv_title=0x7f080009;
         public static final int tv_version_name=0x7f080003;
     }
@@ -79,6 +81,7 @@ containing a value of this type.
         public static final int dialog_confirm_psd=0x7f030003;
         public static final int dialog_set_psd=0x7f030004;
         public static final int gridview_item=0x7f030005;
+        public static final int setting_item_view=0x7f030006;
     }
     public static final class menu {
         public static final int splash=0x7f070000;
--- mobilesafe74/src/com/itheima/mobilesafe74/utils/SpUtil.java
+++ mobilesafe74/src/com/itheima/mobilesafe74/utils/SpUtil.java
@@ -35,6 +35,34 @@ public class SpUtil {
 		sp.edit().putString(key, value).commit();
 		
 	}
+	/**
+	 * 读取boolean标示从sp中
+	 * @param ctx	上下文环境
+	 * @param key	存储节点名称
+	 * @param defValue	没有此节点默认值
+	 * @return		默认值或者此节点读取到的结果
+	 */
+	public static boolean getBoolean(Context ctx,String key,boolean defValue) {
+		if (sp==null) {
+			sp=ctx.getSharedPreferences("config", Context.MODE_PRIVATE);
+		}
+		
+		
+		return sp.getBoolean(key, defValue);
+	}
+	/**
+	 * 写入boolean变量至sp中
+	 * @param ctx	上下文环境
+	 * @param key	存储节点名称
+	 * @param value	存储节点的值 boolean
+	 */
+	public static void putBoolean(Context ctx,String key,boolean value){
+		//(存储节点文件名称,读写方式)
+		if(sp == null){
+			sp = ctx.getSharedPreferences("config", Context.MODE_PRIVATE);
+		}
+		sp.edit().putBoolean(key, value).commit();
+	}
 	
 
 }
